bd451de64e1b6369b7db176fb1676a33c1d30516
viniciuskonig <vinicius.konfer@gmail.com>
2015-07-04 09:00:15 -0300

    111

---
+++ UniRitter.UniRitter2015/Services/ICommentRepository.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UniRitter.UniRitter2015.Models;
+
+namespace UniRitter.UniRitter2015.Services
+{
+    public interface ICommentRepository
+    {
+        CommentModel Add(CommentModel model);
+
+        void Delete(Guid modelId);
+
+        CommentModel Update(Guid id, CommentModel model);
+
+        IEnumerable<CommentModel> GetAll();
+
+        CommentModel GetById(Guid id);
+    }
+}
---
+++ UniRitter.UniRitter2015/Services/Implementation/CommentMemoryRepository.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Web;
+using UniRitter.UniRitter2015.Models;
+
+namespace UniRitter.UniRitter2015.Services.Implementation
+{
+    public class CommentInMemoryRepository : ICommentRepository
+    {
+        private static Dictionary<Guid, CommentModel> data = new Dictionary<Guid, CommentModel>();
+
+        public CommentModel Add(CommentModel model)
+        {
+            var id = Guid.NewGuid();
+            model.id = id;
+            // TODO: this is __NOT__ thread safe!
+            data[id] = model;
+            return model;
+        }
+
+        public void Delete(Guid modelId)
+        {
+            data.Remove(modelId);
+        }
+
+        public CommentModel Update(Guid id, CommentModel model)
+        {
+            // TODO: this is __NOT__ thread safe!
+            // TODO: id should be checked against model.id
+            data[id] = model;
+            return model;
+        }
+
+        public IEnumerable<CommentModel> GetAll()
+        {
+            return data.Values;
+        }
+
+        public CommentModel GetById(Guid id)
+        {
+            return data.ContainsKey(id) ? data[id] : null;
+        }
+    }
+}
